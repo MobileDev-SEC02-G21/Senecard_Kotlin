3e2c74423a694d40a10e1e5e5b3701070b50449b
afgarciab <af.garciab@uniandes.edu.co>
2024-10-02 17:49:39 -0500

    implements borders, text and dinamic qr

--- app/src/main/java/com/mobiles/senecard/QRgenerator/QRViewModel.kt
+++ app/src/main/java/com/mobiles/senecard/QRgenerator/QRViewModel.kt
@@ -1,31 +1,34 @@
 package com.mobiles.senecard.QRgenerator
+
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import android.graphics.Bitmap
 import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.BarcodeEncoder
+import java.util.*
 
 class QRViewModel : ViewModel() {
 
-    // LiveData para el código QR generado (Bitmap)
     private val _qrCodeBitmap = MutableLiveData<Bitmap>()
     val qrCodeBitmap: LiveData<Bitmap> get() = _qrCodeBitmap
 
-    // Método para generar el código QR
+    // Método para generar un QR Code
     fun generateQRCode(data: String) {
         try {
             val barcodeEncoder = BarcodeEncoder()
-            val bitmap: Bitmap = barcodeEncoder.encodeBitmap(
-                data,
-                BarcodeFormat.QR_CODE,
-                750,
-                750
-            )
-            // Asignar el código QR generado al LiveData
+            val bitmap = barcodeEncoder.encodeBitmap(data, BarcodeFormat.QR_CODE, 750, 750)
             _qrCodeBitmap.value = bitmap
         } catch (e: Exception) {
             e.printStackTrace()
         }
+    }
+
+    // Método para generar un QR Code dinámicamente con datos aleatorios
+    fun generateFidelityCardQRCode() {
+
+        val timestamp = System.currentTimeMillis()
+        val message = "Se ha registrado su tarjeta de fidelidad en $timestamp"
+        generateQRCode(message)
     }
 }
--- app/src/main/java/com/mobiles/senecard/QRgenerator/QRgenerator.kt
+++ app/src/main/java/com/mobiles/senecard/QRgenerator/QRgenerator.kt
@@ -1,18 +1,16 @@
 package com.mobiles.senecard.QRgenerator
+
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import com.mobiles.senecard.R
 
 class QRgenerator : AppCompatActivity() {
 
     private lateinit var ivCodigoQR: ImageView
-    private lateinit var etDatos: EditText
-    private lateinit var btnGenerar: Button
 
     // Inicializa el ViewModel
     private val qrViewModel: QRViewModel by viewModels()
@@ -22,8 +20,6 @@ class QRgenerator : AppCompatActivity() {
         setContentView(R.layout.activity_qr_generator)
 
         ivCodigoQR = findViewById(R.id.ivCodigoQR)
-        etDatos = findViewById(R.id.etDatos)
-        btnGenerar = findViewById(R.id.btnGenerar)
 
         // Observa los cambios en el LiveData del ViewModel
         qrViewModel.qrCodeBitmap.observe(this, Observer { bitmap ->
@@ -31,10 +27,7 @@ class QRgenerator : AppCompatActivity() {
             ivCodigoQR.setImageBitmap(bitmap)
         })
 
-        // Al hacer clic en el botón, llama al método para generar el QR en el ViewModel
-        btnGenerar.setOnClickListener {
-            val data = etDatos.text.toString()
-            qrViewModel.generateQRCode(data)
-        }
+        // Genera el código QR dinámicamente al iniciar la actividad
+        qrViewModel.generateFidelityCardQRCode()
     }
 }
