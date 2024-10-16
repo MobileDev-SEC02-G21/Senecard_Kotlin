57318d5523bb259f900c808c27795e205d05e035
afgarciab <af.garciab@uniandes.edu.co>
2024-10-16 11:18:11 -0500

    qr generator according id in firestone

--- app/src/main/java/com/mobiles/senecard/QRgenerator/QRViewModel.kt
+++ app/src/main/java/com/mobiles/senecard/QRgenerator/QRViewModel.kt
@@ -2,17 +2,20 @@ import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.BarcodeEncoder
+import com.mobiles.senecard.model.RepositoryUser
+import kotlinx.coroutines.launch
 
 class QRViewModel : ViewModel() {
 
-    // LiveData para el código QR
+    private val repositoryUser = RepositoryUser.instance
+
     private val _qrCodeBitmap = MutableLiveData<Bitmap>()
     val qrCodeBitmap: LiveData<Bitmap> get() = _qrCodeBitmap
 
-    // LiveData para manejar errores
     private val _error = MutableLiveData<String?>()
     val error: LiveData<String?> get() = _error
 
@@ -34,13 +37,25 @@ class QRViewModel : ViewModel() {
         }
     }
 
-    // Método para generar un QR Code con el UID del usuario
-    fun generateQRCodeWithUID() {
+    // Método para generar un QR Code usando el correo electrónico del usuario autenticado
+    fun generateQRCodeWithEmail() {
         val user = FirebaseAuth.getInstance().currentUser
         if (user != null) {
-            val uid = user.uid
-            val data = "Tu UID es: $uid"
-            generateQRCode(data) // Genera el QR con el UID del usuario
+            val email = user.email // Obtén el correo electrónico del usuario autenticado
+            if (email != null) {
+                viewModelScope.launch {
+                    // Busca al usuario en Firestore usando su correo electrónico
+                    val firestoreUser = repositoryUser.getUserByEmail(email)
+                    if (firestoreUser != null && firestoreUser.id != null) {
+                        // Genera el QR con el ID del usuario encontrado en Firestore
+                        generateQRCode(firestoreUser.id!!)
+                    } else {
+                        _error.value = "Usuario no encontrado en Firestore"
+                    }
+                }
+            } else {
+                _error.value = "Correo electrónico no disponible"
+            }
         } else {
             _error.value = "Usuario no autenticado"
         }
