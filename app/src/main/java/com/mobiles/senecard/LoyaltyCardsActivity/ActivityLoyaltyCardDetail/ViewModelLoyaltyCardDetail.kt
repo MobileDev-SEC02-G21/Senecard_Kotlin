d024494fec7e3a4e9dcfa6478f9d31421c1d2595
afgarciab <af.garciab@uniandes.edu.co>
2024-11-21 20:21:54 -0500

    advance of implementation of swipe refresh in loyaltyCardDetail

--- app/src/main/java/com/mobiles/senecard/LoyaltyCardsActivity/ActivityLoyaltyCardDetail/ViewModelLoyaltyCardDetail.kt
+++ app/src/main/java/com/mobiles/senecard/LoyaltyCardsActivity/ActivityLoyaltyCardDetail/ViewModelLoyaltyCardDetail.kt
@@ -1,17 +1,17 @@
-    package com.mobiles.senecard.viewmodel
+package com.mobiles.senecard.viewmodel
 
-    import androidx.lifecycle.LiveData
-    import androidx.lifecycle.MutableLiveData
-    import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
-    class ViewModelLoyaltyCardDetail : ViewModel() {
+class ViewModelLoyaltyCardDetail : ViewModel() {
 
-        private val _backButtonClicked = MutableLiveData<Boolean>()
-        val backButtonClicked: LiveData<Boolean>
-            get() = _backButtonClicked
+    private val _backButtonClicked = MutableLiveData<Boolean>()
+    val backButtonClicked: LiveData<Boolean>
+        get() = _backButtonClicked
 
-        // Método para manejar el clic en el botón de atrás
-        fun onBackButtonClicked() {
-            _backButtonClicked.value = true
-        }
+    // Método para manejar el clic en el botón de atrás
+    fun onBackButtonClicked() {
+        _backButtonClicked.value = true
     }
+}
