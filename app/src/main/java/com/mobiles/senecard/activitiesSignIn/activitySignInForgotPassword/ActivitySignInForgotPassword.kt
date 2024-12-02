3855fb2c50b89918ecabe4dab08b1549676bbee4
afgarciab <af.garciab@uniandes.edu.co>
2024-12-01 23:45:08 -0500

    implementation of recovery password

--- app/src/main/java/com/mobiles/senecard/activitiesSignIn/activitySignInForgotPassword/ActivitySignInForgotPassword.kt
+++ app/src/main/java/com/mobiles/senecard/activitiesSignIn/activitySignInForgotPassword/ActivitySignInForgotPassword.kt
@@ -1,7 +1,12 @@
 package com.mobiles.senecard.activitiesSignIn.activitySignInForgotPassword
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.util.LruCache
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
@@ -14,6 +19,9 @@ class ActivitySignInForgotPassword : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInForgotPasswordBinding
     private val viewModelSignInForgotPassword: ViewModelSignInForgotPassword by viewModels()
+
+    // LruCache para almacenar el tiempo del último intento
+    private val cache: LruCache<String, Long> = LruCache(1)  // Usamos "1" para un solo valor por ahora.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,37 +38,54 @@ class ActivitySignInForgotPassword : AppCompatActivity() {
             viewModelSignInForgotPassword.backImageViewClicked()
         }
         binding.sendCodeButton.setOnClickListener {
-            viewModelSignInForgotPassword.sendCodeButtonClicked()
+            val email = binding.emailEditText.text.toString()
+
+            if (isInternetAvailable()) {
+                if (canSendEmail()) {
+                    viewModelSignInForgotPassword.sendPasswordReset(email)
+                    updateLastRequestTime()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "You can only request a verification email every 10 minutes.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } else {
+                Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     private fun setObservers() {
-        viewModelSignInForgotPassword.navigateToActivitySignIn.observe(this) { navigate ->
-            if (navigate) {
-                val intent = Intent(this, ActivitySignIn::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
-                val options = ActivityOptionsCompat.makeCustomAnimation(
-                    this,
-                    R.anim.slide_in_left,
-                    R.anim.slide_out_right
-                )
-                finish()
-                startActivity(intent, options.toBundle())
-                viewModelSignInForgotPassword.onNavigated()
+        viewModelSignInForgotPassword.showToastMessage.observe(this) { message ->
+            message?.let {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+                viewModelSignInForgotPassword.onToastShown()
             }
         }
-        viewModelSignInForgotPassword.navigateToActivitySignInVerification.observe(this) { navigate ->
-            if (navigate) {
-                val intent = Intent(this, ActivitySignInVerification::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
-                val options = ActivityOptionsCompat.makeCustomAnimation(
-                    this,
-                    R.anim.slide_in_right,
-                    R.anim.slide_out_left
-                )
-                startActivity(intent, options.toBundle())
-                viewModelSignInForgotPassword.onNavigated()
-            }
-        }
+    }
+
+    private fun canSendEmail(): Boolean {
+        val lastRequestTime = cache.get("last_email_request_time") ?: 0
+        val currentTime = System.currentTimeMillis()
+
+        // Verifica si han pasado al menos 10 minutos (600,000 ms)
+        return (currentTime - lastRequestTime) >= 10 * 60 * 1000
+    }
+
+    private fun updateLastRequestTime() {
+        val currentTime = System.currentTimeMillis()
+        cache.put("last_email_request_time", currentTime)
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+        return networkCapabilities != null &&
+                (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
     }
 }
